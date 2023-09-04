ccabad6fbd646fa3ccfcbdbfa06addc421d6d616
Matt Pfeiffer <Mattpfeiffer54@gmail.com>
2023-09-04 12:21:25 -0500

    Initial Commit

---
+++ SwiftPitchEffect/SwiftPitchEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPitchEffectApp.swift
+//  SwiftPitchEffect
+//
+//  Created by Matt Pfeiffer on 9/4/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPitchEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
